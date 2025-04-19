ae890bebaef6d27fef1eed480ad909016b5f2a38
rockyhg <hrk.haga@gmail.com>
2025-04-20 08:06:42 +0900

    READMEに合わせて更新

--- test_check_df.py
+++ test_check_df.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from check_df import check_df
 
-# サンプル用のCSVファイルを読み込む
-# 適宜、自分のファイルパスに置き換えてください
+# サンプルCSVを読み込み
 df = pd.read_csv("sample_data.csv")
 
-# check_df 関数で情報を取得
+# 関数を実行
 info = check_df(df)
 
 # 結果を表示
